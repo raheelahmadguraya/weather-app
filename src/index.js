bf96a4bed4d727b9a51cdf39a2b9a5212dcc8295
Raheel Ahmad <raheelA0@pm.me>
2022-12-23 15:36:26 -0500

    add CSS styling to geoapify input section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '/Users/raheelahmad/repos/weather-app/node_modules/@geoapify/geocoder-autocomplete/styles/round-borders-dark.css';
+import './styles/round-borders-dark.css';
 import './styles/main.css';
 import { run } from './app/app';
 
